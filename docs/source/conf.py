83b602f3ae5b7ae9cfa4d746feaf08fd9835aa5d
MatheusB4 <62441187+MatheusB4@users.noreply.github.com>
2023-03-03 23:26:53 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,12 @@
+import sphinx_rtd_theme
+
 extensions = ['sphinx.ext.autosectionlabel']
 
-project = 'PCNsim'
+project = 'CACIC-DevKit'
 
+html_theme = 'sphinx_rtd_theme'
+
+html_logo = 'images/CACIC_logo.png'
 html_theme_options = {
-  'logo_name' : 'CACIC-DevKit'
+    'logo_only': True
 }
